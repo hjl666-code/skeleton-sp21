f49d6ace3b49238de194be227a1991d005900d98
sentoayaka <2339221824@qq.com>
2025-12-04 16:08:51 +0800

    1

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,121 @@
+package deque;
+
+import java.util.Iterator;
+
+public class LinkedListDeque<T> implements Iterable<T>, Deque<T> {
+    public Iterator<T> iterator(){
+        return new LinkedListIterator();
+    }
+
+    private class LinkedListIterator implements Iterator<T> {
+        private int cur;
+        public LinkedListIterator(){
+            cur = 0;
+        }
+
+        public boolean hasNext() {
+            return cur < siz;
+        }
+
+        public T next() {
+            return get(cur++);
+        }
+    }
+    public class IntNode<T> {
+        T val;
+        IntNode pre, nxt;
+        public IntNode(){
+            val = null;
+            pre = nxt = null;
+        }
+        public IntNode(T v){
+            val = v;
+            pre = nxt = null;
+        }
+    }
+    IntNode frt, lst;
+    int siz;
+    public LinkedListDeque(){
+        siz = 0;
+    }
+
+    public void addFirst(T item) {
+        if(siz == 0){
+            frt = lst = new IntNode<>(item);
+        }
+        else {
+            IntNode cur = new IntNode<>(item);
+            frt.pre = cur; cur.nxt = frt;
+            frt = cur;
+        }
+        siz ++;
+    }
+
+    public void addLast(T item) {
+        if(siz == 0){
+            frt = lst = new IntNode<>(item);
+        }
+        else {
+            IntNode cur = new IntNode<>(item);
+            lst.pre = cur; cur.pre = lst;
+            lst = cur;
+        }
+        siz ++;
+    }
+
+    public boolean isEmpty() {
+        if(siz == 0)return false;
+        else return true;
+    }
+
+    public int size() {
+        return siz;
+    }
+
+    public void printDequeue() {
+        IntNode p = frt;
+        System.out.print(p.val);
+        System.out.print(" ");
+        while(p != lst){
+            p = p.nxt;
+            System.out.print(p.val);
+            System.out.print(" ");
+        }
+        System.out.print("\n");
+    }
+
+    public T removeFirst() {
+        siz --; IntNode<T> cur = frt;
+        if(siz == 0)frt = lst = null;
+        else {frt = frt.nxt; frt.pre = null;}
+        return cur.val;
+    }
+
+    public T removeLast() {
+        siz --; IntNode<T> cur = frt;
+        if(siz == 0)lst = lst = null;
+        else {lst = lst.pre; lst.nxt = null;}
+        return cur.val;
+    }
+
+    public T get(int index) {
+        if(index >= siz) return null;
+        IntNode<T> p = frt;
+        while(index != 0){
+            index --;
+            p = p.nxt;
+        }
+        return p.val;
+    }
+    public T getRecursive(int index) {
+        if(index >= siz) return null;
+        class getHelper{
+            public T helper(IntNode p, int index){
+                if(index == 0) return (T) p.val;
+                else return (T) helper(p.nxt, index - 1);
+            }
+        }
+        getHelper getHelp = new getHelper();
+        return getHelp.helper(frt, index);
+    }
+}
